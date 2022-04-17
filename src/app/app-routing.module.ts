0d006adc5d0aadd23a7bf95a7455b9dd421a7ce5
Datnguyen <datndd2604@gmail.com>
2022-04-17 10:56:01 +0700

    web sim dien thoai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from 'src/core/dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { useHash: false, relativeLinkResolution: 'corrected' }),
+  ],
 })
 export class AppRoutingModule { }
